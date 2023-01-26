772e1c0b199f324d4e8bce438bd4e218fc3781cc
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-01-26 12:33:50 -0300

    newid

--- data-base.js
+++ data-base.js
@@ -1,7 +1,8 @@
 let candidatos = [
     
     {numero: 13, nome: 'Lulu Costa', imagem: '<img src="fotos/lulu.png" alt="">' },
-    {numero: 22, nome: 'Maxima Felp', imagem: '<img src="fotos/maxima.png" alt="">'}
+    {numero: 22, nome: 'Maxima Felp', imagem: '<img src="fotos/maxima.png" alt="">'},
+    {numero:'', nome:'DIGITE 13 ou 22', imagem:'<img src="http://robohash.org/123">'},
 
 ]
 
